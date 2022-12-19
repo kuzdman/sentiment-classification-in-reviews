08a9194a94042187e41e88289176c2a03221ce58
kuzdman <dmitriykuznetsov23@gmail.com>
2022-12-19 21:33:08 +0500

    first release

--- main.py
+++ main.py
@@ -3,16 +3,25 @@
 import pandas as pd
 import numpy as np
 
+from keras.models import load_model
+
 from preprocessing import fit_tokenizer, preprocessing
+from training import train_model
 
 
 def main():
-    x_train = pd.read_csv('data/train.csv')
+    text = [
+        "The coffee was not as good as I wanted. I don't understand why it's so bitter.",
+        "The food is amazing! Beautiful place! I recommend!"
+    ]
+    text = pd.Series(np.array(text))
+    text = preprocessing(text)
 
-    # fit_tokenizer(x_train.Review, 50000)
-    # text = preprocessing(pd.Series(np.array(['The coffee was not as good as I wanted'])), 50)
+    model = load_model('model')
+    predictions = model.predict(text)
 
-    # print(text)
+    for i in range(len(predictions)):
+        print(f"Review {i + 1} -> {round(predictions[i, 0] * 100, 1)}% good review")
 
 
 if __name__ == "__main__":
